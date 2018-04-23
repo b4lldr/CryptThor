4b9f3543fc71836c1b458739d7711b14f1b942ac
DrBall <drbip9@gmail.com>
2018-04-23 15:13:42 +0200

    Finnished draging and stuff around that

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,11 +23,39 @@ namespace CryptThor
         public MainWindow()
         {
             InitializeComponent();
+
+            MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth; 
         }
 
-        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void btnHelp_Click(object sender, RoutedEventArgs e)
         {
-            DragMove();
+
+        }
+
+        private void btnSett_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMin_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnMax_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            imgMax.Icon = WindowState == WindowState.Maximized ? FontAwesome.WPF.FontAwesomeIcon.WindowRestore : FontAwesome.WPF.FontAwesomeIcon.WindowMaximize;
         }
     }
 }
